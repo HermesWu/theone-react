354673e73f0abeebed01d65a176f684d65dba4a0
HermesWu <1136025519@qq.com>
2019-07-11 17:43:32 +0800

    添加 webpack.config.prod.js webpack.config.dev.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  mode: 'production',
   entry: {
     theRone: './lib/index.tsx'
   },
@@ -20,25 +18,5 @@ module.exports = {
         loader: 'awesome-typescript-loader'
       }
     ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'the-onen',
-      template: 'index.html'
-    })
-  ],
-  externals: {
-    react: {
-      commonjs: 'react',
-      commonjs2: 'react',
-      amd: 'react',
-      root: 'React',
-    },
-    'react-dom': {
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom',
-      root: 'ReactDOM',
-    },
   }
 };
