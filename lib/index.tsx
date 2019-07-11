f89f1b3adf38c04b75ecc33fd02361feb3d7af1e
HermesWu <1136025519@qq.com>
2019-07-11 17:58:39 +0800

    完成 tsconfig.json 配置

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,1 +1,6 @@
-console.log(1);
+// import * as React from 'react'
+import * as React from 'react';
+import * as ReactDom from 'react-dom';
+import Button from './button';
+
+ReactDom.render(<Button />, document.body);
