9750db064d8d6d457314a619e8f4b6c175395a13
HermesWu <1136025519@qq.com>
2019-07-11 18:54:31 +0800

    完善测试用例

--- lib/__tests__/hello.unit.tsx
+++ lib/__tests__/hello.unit.tsx
@@ -1,5 +1,9 @@
+function add(a: number, b: number) {
+    return a + b;
+}
+
 describe('我的第一个测试用例', () => {
-    it('1 等于 1', () => {
-        expect(1).toEqual(2);
+    it('add(1, 2) 等于 3', () => {
+        expect(add(1, 2)).toEqual(3);
     });
 });
