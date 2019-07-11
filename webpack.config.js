774419077722cbd1d2b3cbb94d31cded2166ccdf
HermesWu <1136025519@qq.com>
2019-07-11 17:08:10 +0800

    配置 webpack.config.js externals

--- webpack.config.js
+++ webpack.config.js
@@ -26,5 +26,19 @@ module.exports = {
       title: 'the-onen',
       template: 'index.html'
     })
-  ]
+  ],
+  externals: {
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'react',
+      root: 'React',
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'react-dom',
+      root: 'ReactDOM',
+    },
+  }
 };
