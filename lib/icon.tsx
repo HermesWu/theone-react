6867ed0672819ff420de9c9b58f5f3585b6f4fd5
HermesWu <1136025519@qq.com>
2019-10-05 17:27:16 +0800

    add scss for Icon Component

--- lib/icon.tsx
+++ lib/icon.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './importIcons';
+import './icon.scss'
 
 
 interface IconProps {
@@ -8,11 +9,9 @@ interface IconProps {
 
 const icon: React.FunctionComponent<IconProps> = (props) => {
     return (
-        <span>
-            <svg>
-                <use xlinkHref={`#${props.name}`}/>
-            </svg>
-        </span>
+        <svg className="theonen-icon">
+            <use xlinkHref={`#${props.name}`}/>
+        </svg>
     );
 };
 
