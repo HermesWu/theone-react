082334aac3a30932b83f5e12e41452509683ce1b
HermesWu <1136025519@qq.com>
2019-10-07 21:12:52 +0800

    bug fixed jest.config.ci.js

--- jest.config.ci.js
+++ jest.config.ci.js
@@ -2,7 +2,7 @@ const base = require('./jest.config')
 module.exports = Object.assign({}, base, {
   reporters: ["jest-junit"],
   collectCoverage: true,
-  collectCoverageFrom: ["lib/**/(.{ts,tsx}", "!**/node_modules/**"],
+  collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
   coverageDirectory: 'coverage',
   coverageReporters: ['text', 'lcov']
 })
