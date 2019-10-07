652b49b672ae20e7e6fd808fa982513d5db8e3e1
HermesWu <1136025519@qq.com>
2019-10-07 23:19:24 +0800

    bug fixed circle ci

--- jest.config.ci.js
+++ jest.config.ci.js
@@ -1,8 +1,8 @@
 const base = require('./jest.config')
 module.exports = Object.assign({}, base, {
-  reporters: ["default","jest-junit"],
+  reporters: ["jest-junit"],
   collectCoverage: true,
   collectCoverageFrom: ["lib/**/*.{ts,tsx}", "!**/node_modules/**"],
   coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov']
+  coverageReporters: ['text', 'lcov'],
 })
