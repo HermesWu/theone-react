3a7108f89cd5a12510b56502e01813dc736be035
HermesWu <1136025519@qq.com>
2019-10-08 12:34:21 +0800

    add export component

--- lib/index.tsx
+++ lib/index.tsx
@@ -1,16 +1,2 @@
-// import * as React from 'react'
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
-import Icon from './icon/icon'
 
-const fn:React.MouseEventHandler = (e) => {
-        console.log(e.target)
-}
-
-ReactDom.render(<div>
-        <Icon name="wechat"  onClick={fn}/>
-        <Icon name="alipay" onMouseEnter={(e)=>{console.log('enter');console.log(e.target)}}/>
-        <Icon name="qq" />
-</div>
-    , document.querySelector('#root')
-);
+export {default as Icon}  from './icon/icon';
