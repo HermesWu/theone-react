e2524e85659ebe8ec5d481be0e00f23c76bc77d4
HermesWu <1136025519@qq.com>
2019-10-08 21:08:10 +0800

    add button.example.tsx

---
+++ lib/button.example.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import {Icon} from './index';
+
+const ButtonExample: React.FunctionComponent = () => {
+  return (
+    <div>
+      111
+      <Icon name="alipay"/>
+      <Icon name="wechat"/>
+      <Icon name="qq"/>
+    </div>
+  );
+};
+
+export default ButtonExample;
