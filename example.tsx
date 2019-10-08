492c92c2ee16c45dd2a0ddaafc82df044c08deb3
HermesWu <1136025519@qq.com>
2019-10-08 21:07:43 +0800

    add example.tsx

--- example.tsx
+++ example.tsx
@@ -1,8 +1,32 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
+import {HashRouter as Router, Route, Link} from 'react-router-dom';
+import IconExample from './lib/icon/icon.example';
+import ButtonExample from './lib/button.example';
 
 ReactDom.render(
+  <Router>
     <div>
-        hi
-    </div>,
-    document.querySelector('#root'));
+      <header>
+        <div className="logo">The-One</div>
+      </header>
+    </div>
+    <div>
+      <aside>
+        <h2>组件</h2>
+        <ul>
+          <li>
+            <Link to="/icon">Icon</Link>
+          </li>
+          <li>
+            <Link to="/button">Button</Link>
+          </li>
+        </ul>
+      </aside>
+      <main>
+        <Route path="/icon" component={IconExample}/>
+        <Route path="/button" component={ButtonExample}/>
+      </main>
+    </div>
+  </Router>,
+  document.querySelector('#root'));
