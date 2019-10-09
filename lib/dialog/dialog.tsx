67dccd43c866cf37e7c469392dc17de89a469238
HermesWu <1136025519@qq.com>
2019-10-09 21:53:20 +0800

    add dialog.tsx

---
+++ lib/dialog/dialog.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface Props {
+  visible: boolean
+}
+
+const Dialog: React.FunctionComponent<Props> = (props) => {
+  return (
+    props.visible ?
+      <div>dialog</div> :
+      null
+  );
+};
+
+export default Dialog;
