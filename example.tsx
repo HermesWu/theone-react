f6fd114bcb86fcfa1cdd3036bcae8a6b8f371b23
HermesWu <1136025519@qq.com>
2019-10-09 21:54:23 +0800

    add dialog router

--- example.tsx
+++ example.tsx
@@ -3,6 +3,7 @@ import * as ReactDom from 'react-dom';
 import {HashRouter as Router, Route, Link} from 'react-router-dom';
 import IconExample from './lib/icon/icon.example';
 import ButtonExample from './lib/button.example';
+import DialogExample from './lib/dialog/dialog.example';
 
 ReactDom.render(
   <Router>
@@ -21,11 +22,15 @@ ReactDom.render(
           <li>
             <Link to="/button">Button</Link>
           </li>
+          <li>
+            <Link to="/dialog">对话框</Link>
+          </li>
         </ul>
       </aside>
       <main>
         <Route path="/icon" component={IconExample}/>
         <Route path="/button" component={ButtonExample}/>
+        <Route path="/dialog" component={DialogExample}/>
       </main>
     </div>
   </Router>,
