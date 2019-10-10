4301e52e5b126a1e349f92d883ec708e5e3f0b2c
HermesWu <1136025519@qq.com>
2019-10-10 21:20:44 +0800

    optimised dialog.example.tsx for EventHandler

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,14 +1,38 @@
-import React,{useState} from 'react';
+import React, {Fragment, useState} from 'react';
 import Dialog from './dialog';
 
 
 const DialogExample: React.FunctionComponent = () => {
-  const [x, setX] = useState(false)
+  const [x, setX] = useState(false);
+  const [y, setY] = useState(false);
   return (
-    <div>
-      <button onClick={()=>{setX(!x)}}>click</button>
-      <Dialog visible={ x }/>
-    </div>
+    <Fragment>
+      <div>
+        <button onClick={() => {setX(!x);}}>click</button>
+        <Dialog visible={x} buttons={
+          [
+            <button onClick={() => {setX(false);}}>ok</button>,
+            <button onClick={() => {setX(false);}}>cancel</button>
+          ]}
+                onClose={() => {setX(false);}}
+        >
+          <div>hi</div>
+        </Dialog>
+      </div>
+      <div>
+        <button onClick={() => {setY(!y);}}>click</button>
+        <Dialog visible={y} closeOnClickMask={true} buttons={
+          [
+            <button onClick={() => {setY(false);}}>ok</button>,
+            <button onClick={() => {setY(false);}}>cancel</button>
+          ]}
+                onClose={() => {setY(false);}}
+        >
+          <div>hi</div>
+        </Dialog>
+      </div>
+    </Fragment>
+
   );
 };
 
