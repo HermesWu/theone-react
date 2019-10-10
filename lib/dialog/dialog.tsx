56b53738f8f4bc2d4f40d4cae554f1fe2b6e919d
HermesWu <1136025519@qq.com>
2019-10-10 19:25:33 +0800

    add Dialog base DOM

--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,13 +1,37 @@
 import * as React from 'react';
+import {ReactElement} from 'react';
+import './dialog.scss'
+import {scopedClassMaker} from '../helps/classes';
+import {Icon} from '../index';
 
 interface Props {
-  visible: boolean
+  visible: boolean,
+  buttons?: Array<ReactElement>
 }
+
+const scopedClass = scopedClassMaker('theone-dialog')
+const sc = scopedClass
 
 const Dialog: React.FunctionComponent<Props> = (props) => {
   return (
     props.visible ?
-      <div>dialog</div> :
+      <div>
+        <div className={sc('mask')}/>
+        <div className={sc('')}>
+          <div className={sc("close")}>
+            <Icon name="close"></Icon>
+          </div>
+          <header className={sc('header')}>
+            提示
+          </header>
+          <main className={sc('main')}>
+            <div>{props.children}</div>
+          </main>
+          <footer className={sc('footer')}>
+            {props.buttons}
+          </footer>
+        </div>
+      </div> :
       null
   );
 };
