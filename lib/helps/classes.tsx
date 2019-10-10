1ae03c33f3025aa6e096336d3a439ac085211d4d
HermesWu <1136025519@qq.com>
2019-10-10 18:43:29 +0800

    add scopedClassMaker to classes.tsx

--- lib/helps/classes.tsx
+++ lib/helps/classes.tsx
@@ -1,5 +1,12 @@
-function classes(...names:(string|undefined)[]){
-    return names.filter(Boolean).join(' ')
+function classes(...names: (string | undefined)[]) {
+  return names.filter(Boolean).join(' ');
 }
 
-export default classes
+function scopedClassMaker(prefix: string) {
+  return function(name:string){
+    return [prefix,name].filter(Boolean).join('-')
+  }
+}
+
+export default classes;
+export {scopedClassMaker};
