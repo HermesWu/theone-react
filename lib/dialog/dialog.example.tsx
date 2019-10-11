16e97b3c79eb6b14676ae11d6df9f4363c9a1621
HermesWu <1136025519@qq.com>
2019-10-11 11:01:57 +0800

    add confirm & modal API for Dialog

--- lib/dialog/dialog.example.tsx
+++ lib/dialog/dialog.example.tsx
@@ -1,17 +1,27 @@
 import React, {Fragment, useState} from 'react';
-import Dialog, {alert} from './dialog';
+import Dialog, {alert, confirm, modal} from './dialog';
 
 
 const DialogExample: React.FunctionComponent = () => {
   const [x, setX] = useState(false);
   const [y, setY] = useState(false);
+  const openModal = () => {
+    const close = modal(<div>你好 <button onClick={()=>close()}>close</button></div>)
+  }
   return (
     <Fragment>
       <div>
-        <h1>Example 3</h1>
-        <button onClick={()=>alert('1')}>alert</button>
+        <h1>Example 4</h1>
+        <button onClick={() => confirm('1', () => {console.log('你点击了yes');}, () => {console.log('你点击了no')})}>
+          confirm
+        </button>
+        <button onClick={openModal}>confirm</button>
       </div>
-      <div style={{position: 'relative', zIndex: 10, border: '1px solid red', color:'red'}}>
+      <div>
+        <h1>Example 3</h1>
+        <button onClick={() => alert('1')}>alert</button>
+      </div>
+      <div style={{position: 'relative', zIndex: 10, border: '1px solid red', color: 'red'}}>
         <h1>Example 1</h1>
         <button onClick={() => {setX(!x);}}>click</button>
         <Dialog visible={x} buttons={
@@ -24,7 +34,7 @@ const DialogExample: React.FunctionComponent = () => {
           <div>hi</div>
         </Dialog>
       </div>
-      <div style={{position: 'relative', zIndex:9}}>
+      <div style={{position: 'relative', zIndex: 9}}>
         <h1>Example 2</h1>
         <button onClick={() => {setY(!y);}}>click</button>
         <Dialog visible={y} closeOnClickMask={true} buttons={
--- lib/dialog/dialog.tsx
+++ lib/dialog/dialog.tsx
@@ -1,4 +1,4 @@
-import React, {Fragment, ReactElement} from 'react';
+import React, {Fragment, ReactElement, ReactFragment, ReactNode} from 'react';
 import './dialog.scss';
 import {scopedClassMaker} from '../helps/classes';
 import {Icon} from '../index';
@@ -42,7 +42,7 @@ const Dialog: React.FunctionComponent<Props> = (props) => {
           )}
         </footer>
       </div>
-    </Fragment>
+    </Fragment>;
   return (
     ReactDOM.createPortal(result, document.body)
   );
@@ -51,16 +51,61 @@ Dialog.defaultProps = {
   closeOnClickMask: false
 };
 
-const alert = (content:string) => {
+const alert = (content: string) => {
   const component = <Dialog onClose={() => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+  }} visible={true}>{content}</Dialog>;
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  ReactDOM.render(component, div);
+};
+
+const confirm = (content: string, yes?: () => void, no?: () => void) => {
+  const onClose = () => {
+    ReactDOM.render(React.cloneElement(component, {visible: false}), div);
+    ReactDOM.unmountComponentAtNode(div);
+    div.remove();
+  };
+  const onOk = () => {
+    onClose();
+    yes && yes();
+  };
+  const onNo = () => {
+    onClose();
+    no && no();
+  };
+  const component = (
+    <Dialog
+      onClose={onClose}
+      buttons={[<button onClick={onOk}>ok</button>, <button onClick={onNo}>cancel</button>]}
+      visible={true}>
+      {content}
+    </Dialog>);
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  ReactDOM.render(component, div);
+};
+
+const modal = (content: ReactNode | ReactFragment) => {
+  const onClose = () => {
     ReactDOM.render(React.cloneElement(component, {visible: false}), div)
     ReactDOM.unmountComponentAtNode(div)
     div.remove()
-  }} visible={true}>{content}</Dialog>
+  }
+  const component = (
+    <Dialog
+      onClose={onClose}
+      visible={true}>
+      {content}
+    </Dialog>
+  );
   const div = document.createElement('div')
   document.body.appendChild(div)
   ReactDOM.render(component, div)
-}
+  return onClose
+};
 
 export default Dialog;
-export {alert}
+export {alert, confirm, modal};
