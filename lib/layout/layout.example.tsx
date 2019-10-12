3cafd9a9c4d8f740c2c4822c3c63a838af949b5b
HermesWu <1136025519@qq.com>
2019-10-12 13:09:23 +0800

    add: check children type layout.tsx

--- lib/layout/layout.example.tsx
+++ lib/layout/layout.example.tsx
@@ -1,18 +1,63 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 import Layout from './layout';
 import Header from './header';
 import Content from './content';
 import Footer from './footer';
+import Aside from './aside';
 
 const LayoutExample: React.FunctionComponent = () => {
   return (
-    <div>
-      <Layout>
-        <Header>header</Header>
-        <Content>content</Content>
-        <Footer>footer</Footer>
-      </Layout>
-    </div>
+    <Fragment>
+      <div>
+        <h1>布局一</h1>
+        <Layout style={{height: '500px'}}>
+          <Header>header</Header>
+          <Content>content</Content>
+          <Footer>footer</Footer>
+        </Layout>
+      </div>
+      <div>
+        <h1>布局二</h1>
+        <Layout style={{height: '500px'}}>
+          <Header>header</Header>
+          <Layout>
+            <Aside>aside</Aside>
+            <Content>
+              content
+            </Content>
+          </Layout>
+          <Footer>footer</Footer>
+        </Layout>
+      </div>
+      <div>
+        <h1>布局三</h1>
+        <Layout style={{height: '500px'}}>
+          <Header>header</Header>
+          <Layout>
+            <Content>
+              content
+            </Content>
+            <Aside>aside</Aside>
+          </Layout>
+          <Footer>footer</Footer>
+        </Layout>
+      </div>
+      <div>
+        <h1>布局四</h1>
+        <Layout style={{height: '500px'}}>
+          <Aside>aside</Aside>
+          <Layout>
+            <Header>header</Header>
+            <Content>
+              content
+            </Content>
+            <Footer>footer</Footer>
+          </Layout>
+
+        </Layout>
+      </div>
+    </Fragment>
+
   );
 };
 
--- lib/layout/layout.tsx
+++ lib/layout/layout.tsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {ReactElement} from 'react';
 import {scopedClassMaker} from '../helps/classes';
+import './layout.scss'
+import Aside from './aside';
 
 interface Props extends React.HTMLAttributes<HTMLElement>{
-
+  children: ReactElement | Array<ReactElement>
 }
-
 const sc = scopedClassMaker('theone-layout')
 
 const Layout: React.FunctionComponent<Props> = (props) => {
   const {className, ...restProps} = props
+
+  let hasAside = false
+
+  console.log(props.children);
+  (props.children as Array<ReactElement>).map(node => {
+    if(node.type === Aside){
+      hasAside = true
+    }
+  })
+
   return (
-    <div className={sc('', {extra: className})} {...restProps}>
+    <div className={sc('', {extra: [className, hasAside && 'hasAside'].join(' ')})} {...restProps}>
       {props.children}
     </div>
   );
