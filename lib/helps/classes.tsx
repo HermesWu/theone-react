e88fd09a0aacde7410f5f69693121cad4ad4b933
HermesWu <1136025519@qq.com>
2019-10-12 21:14:38 +0800

    Refactor: classes.tsx

--- lib/helps/classes.tsx
+++ lib/helps/classes.tsx
@@ -12,13 +12,15 @@ interface ClassToggles{
 
 function scopedClassMaker(prefix: string) {
   return function (name: string | ClassToggles, options?: Options) {
-    let result
-    if(typeof name === 'string' || typeof name === undefined){
-      result = [prefix, name].filter(Boolean).join('-');
-    }else{
-      result = Object.entries(name).filter(kv => kv[1]).map(kv => kv[0])
-        .map(n => [prefix, n].filter(Boolean).join('-')).join(' ')
-    }
+    const result = Object
+      .entries(name instanceof Object ? name: {[name]:true})
+      .filter(kv => kv[1])
+      .map(kv => kv[0])
+      .map(name => [prefix, name]
+        .filter(Boolean)
+        .join('-'))
+      .join(' ')
+
     if (options && options.extra) {
       return [result, options && options.extra].filter(Boolean).join(' ');
     } else {
