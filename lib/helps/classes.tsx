713648720d028045d6fa81589c202c257cccbfa6
HermesWu <1136025519@qq.com>
2019-10-12 21:32:23 +0800

    Refactor: classes.tsx 2

--- lib/helps/classes.tsx
+++ lib/helps/classes.tsx
@@ -6,28 +6,22 @@ interface Options {
   extra: string | undefined
 }
 
-interface ClassToggles{
+interface ClassToggles {
   [k: string]: boolean
 }
 
-function scopedClassMaker(prefix: string) {
-  return function (name: string | ClassToggles, options?: Options) {
-    const result = Object
-      .entries(name instanceof Object ? name: {[name]:true})
+const scopedClassMaker = (prefix: string) =>
+  (name: string | ClassToggles, options?: Options) =>
+    Object
+      .entries(name instanceof Object ? name : {[name]: true})
       .filter(kv => kv[1])
       .map(kv => kv[0])
       .map(name => [prefix, name]
         .filter(Boolean)
         .join('-'))
-      .join(' ')
+      .concat(options && options.extra || [])
+      .join(' ');
 
-    if (options && options.extra) {
-      return [result, options && options.extra].filter(Boolean).join(' ');
-    } else {
-      return result;
-    }
-  };
-}
 
 export default classes;
 export {scopedClassMaker};
