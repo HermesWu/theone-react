fc11c7f482ef7074f7df9bb21bedcbd21df1af83
HermesWu <1136025519@qq.com>
2019-10-12 20:57:09 +0800

    optimised: classes.unit.jsx for scopedClassMaker

--- lib/helps/__tests__/classes.unit.jsx
+++ lib/helps/__tests__/classes.unit.jsx
@@ -1,4 +1,4 @@
-import classes from '../classes';
+import classes, {scopedClassMaker} from '../classes';
 
 describe('classes', () => {
   it('接受 1 个 className', () => {
@@ -22,3 +22,13 @@ describe('classes', () => {
     expect(result).toEqual('a 晴天');
   });
 });
+describe('scopedClassMaker', () => {
+  it('接受字符串或对象', () => {
+    const sc = scopedClassMaker('theone-test')
+    expect(sc('')).toEqual('theone-test');
+    expect(sc('x')).toEqual('theone-test-x');
+    expect(sc({y: true, z: false})).toEqual('theone-test-y');
+    expect(sc({y: true, z: true})).toEqual('theone-test-y theone-test-z')
+    expect(sc({y: true, z: true}, {extra: 'red'})).toEqual('theone-test-y theone-test-z red')
+  })
+})
