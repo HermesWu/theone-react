9ac0db9b686b87f46ee0ffaef0540f6f670d3e32
HermesWu <1136025519@qq.com>
2019-10-12 18:28:35 +0800

    optimised: scopedClassMaker multiple arguments

--- lib/helps/classes.tsx
+++ lib/helps/classes.tsx
@@ -6,9 +6,19 @@ interface Options {
   extra: string | undefined
 }
 
+interface ClassToggles{
+  [k: string]: boolean
+}
+
 function scopedClassMaker(prefix: string) {
-  return function (name: string, options?: Options) {
-    const result = [prefix, name].filter(Boolean).join('-');
+  return function (name: string | ClassToggles, options?: Options) {
+    let result
+    if(typeof name === 'string' || typeof name === undefined){
+      result = [prefix, name].filter(Boolean).join('-');
+    }else{
+      result = Object.entries(name).filter(kv => kv[1]).map(kv => kv[0])
+        .map(n => [prefix, n].filter(Boolean).join('-')).join(' ')
+    }
     if (options && options.extra) {
       return [result, options && options.extra].filter(Boolean).join(' ');
     } else {
