5af778f38ea936d6ab6f8be242c6742521f0f55f
HermesWu <1136025519@qq.com>
2019-10-27 22:38:31 +0800

    refactor: validator.tsx

--- lib/form/validator.tsx
+++ lib/form/validator.tsx
@@ -32,47 +32,57 @@ const validator = (formValue: FormValue, rules: FormRules, callback: (errors: an
   rules.map(rule => {
     const value = formValue[rule.key];
     if (rule.validator) {
-      const promise = rule.validator(value);
-      addErrors(rule.key, {message:rule.validator.name, promise:promise});
+      const promise = rule.validator(value)
+      addErrors(rule.key, {message: rule.validator.name, promise: promise});
     }
     if (rule.required && isEmpty(value)) {
-      addErrors(rule.key, {message:'required'});
+      addErrors(rule.key, {message: 'required'});
     }
     if (rule.minLength && !isEmpty(value) && value.length < rule.minLength) {
-      addErrors(rule.key, {message:'minLength'});
+      addErrors(rule.key, {message: 'minLength'});
     }
     if (rule.maxLength && !isEmpty(value) && value.length > rule.maxLength) {
-      addErrors(rule.key, {message:'maxLength'});
+      addErrors(rule.key, {message: 'maxLength'});
     }
     if (rule.pattern && !isEmpty(value) && !rule.pattern.test(value)) {
-      addErrors(rule.key, {message:'pattern'});
+      addErrors(rule.key, {message: 'pattern'});
     }
   });
-  const promiseList = flat(Object.values(errors)).filter(item=>item.promise).map(item=>item.promise)
-Promise.all(promiseList)
-  .finally(()=>{
-    // 对errors 进行处理，剔除promise，重新组合成正确格式
-    // console.log(errors)
-    const newErrors = fromEntries(
-      Object.keys(errors)
-        .map<[string, string[]]>(key=> [key,errors[key].map((item: OneError)=>item.message)]))
-    callback(newErrors)
-  })
+  const promiseList = flat<OneError|OneError[]>(Object.values(errors)).filter(item => item.promise).map(item => item.promise);
+  Promise.all(promiseList)
+    .finally(() => {
+      // 对errors 进行处理，剔除promise，重新组合成正确格式
+      // console.log(errors)
+      const newErrors = fromEntries(
+        Object.keys(errors)
+          .map<[string, string[]]>(key => [key, errors[key].map((item: OneError) => item.message)]));
+      callback(newErrors);
+    });
 };
 
 export default validator;
 
-function flat(array: Array<any>) {
-  const result = [];
+function flat<T>(array: Array<T | T[]>) {
+  const result: T[] = [];
   for (let i = 0; i < array.length; i++) {
     if (array[i] instanceof Array) {
-      result.push(...array[i]);
+      result.push(...array[i] as T[]);
     } else {
-      result.push(array[i]);
+      result.push(array[i] as T);
     }
   }
   return result;
 }
+
+function zip(kvList: Array<[string, string]>) {
+  const result: { [key: string]: string[] } = {};
+  kvList.map(([key, value]) => {
+    result[key] = result[key] || [];
+    result[key].push(value);
+  });
+  return result;
+}
+
 function fromEntries(array:Array<[string, string[]]>){
   const result:{[key:string]: string[]} = {}
   for(let i = 0; i < array.length; i++){
