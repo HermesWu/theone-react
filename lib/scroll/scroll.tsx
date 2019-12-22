36030dd39e73ed8567c73aa89547fcaaf1f765f7
HermesWu <1136025519@qq.com>
2019-12-22 15:16:20 +0800

    add: scroll bar for scroll.tsx

--- lib/scroll/scroll.tsx
+++ lib/scroll/scroll.tsx
@@ -1,24 +1,52 @@
-import * as React  from 'react';
-import { HTMLAttributes } from 'react';
-import './scroll.scss'
+import * as React from 'react';
+import {HTMLAttributes, UIEventHandler, useState, useEffect, useRef} from 'react';
 import {scopedClassMaker} from '../helps/classes';
 import scrollbarWidth from './scroll-width';
+import './scroll.scss';
 
-interface Props extends HTMLAttributes<HTMLDivElement>{}
-const scopedClass = scopedClassMaker('theone-scroll')
-const sc = scopedClass
+
+interface Props extends HTMLAttributes<HTMLDivElement> {}
+
+const scopedClass = scopedClassMaker('theone-scroll');
+const sc = scopedClass;
 
 const Scroll: React.FunctionComponent<Props> = (props) => {
-  const {children, ...rest} = props
-  return(
+  const {children, ...rest} = props;
+
+  const [barHeight, setBarHeight] = useState(0);
+  const [scrollTop, setScrollTop] = useState(0)
+  const containerRef = useRef<HTMLDivElement>(null)
+
+  const onScroll: UIEventHandler = (e) => {
+    const {current} = containerRef
+    const scrollTop = current!.scrollTop
+    const scrollHeight = current!.scrollHeight;
+    const viewHeight = current!.getBoundingClientRect().height;
+    setScrollTop(scrollTop * viewHeight / scrollHeight)
+
+  };
+  useEffect(() => { // mounted 的时候计算滚动条高度
+    const scrollHeight = containerRef.current!.scrollHeight
+    const viewHeight = containerRef.current!.getBoundingClientRect().height
+    setBarHeight(viewHeight * viewHeight / scrollHeight)
+  }, []);
+  return (
     <div className={sc('')} {...rest}>
-      <div className={sc('inner')} style={{right: -scrollbarWidth()}}>
+      <div
+        className={sc('inner')}
+        style={{right: -scrollbarWidth()}}
+        ref={containerRef}
+        onScroll={onScroll}
+      >
         {children}
       </div>
+      <div className={sc('track')}>
+        <div className={sc('bar')} style={{height: barHeight, transform:`translateY(${scrollTop}px)`}}></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Scroll
+export default Scroll;
 
 
